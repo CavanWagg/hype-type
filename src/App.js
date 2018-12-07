63f915d1aca381e18a9f74441c423a8691816848
Cavan Wagg <waggx002@gmail.com>
2018-12-07 14:58:31 -0600

    styled-components, simple grid layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  *,
+  *:before,
+  *:after {
+  box-sizing: border-box;
+  }
+  body {
+    margin: 40px;
+    font-family: 'Open Sans', 'sans-serif';
+    background-color: #fff;
+    color: rgb(38, 17, 173);
+  }
+  h1,
+  p {
+  margin: 0 0 1em 0;
+  }
+  @supports (display: grid) {
+    .wrapper > * {
+    width: auto;
+    margin: 0;
+    }
+  }
+  .wrapper > * {
+    background-color: #444;
+    color: #fff;
+    border-radius: 5px;
+    padding: 20px;
+    font-size: 150%;
+    /* needed for the floated layout*/
+    margin-bottom: 10px;
+  }
+  `;
+
+const AppWrapper = styled.div`
+  @media screen and (min-width: 500px) {
+    margin: 0 auto;
+    grid-template-columns: 1fr 5fr;
+  }
+  max-width: 1200px;
+  margin: 0 20px;
+  display: grid;
+  grid-gap: 10px;
+`;
+
+const AppHeader = styled.header`
+  @media screen and (min-width: 500px) {
+    grid-column: 1 / -1;
+    /* needed for the floated layout */
+    clear: both;
+  }
+`;
+
+const AppContent = styled.article`
+  @media screen and (min-width: 500px) {
+    float: right;
+    width: 79.7872%;
+    min-height: 500px;
+  }
+`;
+
+const AppSidebar = styled.aside`
+  @media screen and (min-width: 500px) {
+    float: left;
+    /* width: 19.1489%; */
+  }
+`;
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppWrapper className="wrapper">
+        <AppHeader> Hype Type</AppHeader>
+        <AppSidebar> Score </AppSidebar>
+        <AppContent>Game Content</AppContent>
+        {/* <footer className="footer">My footer</footer> */}
+        <GlobalStyle />
+      </AppWrapper>
     );
   }
 }
